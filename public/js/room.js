74b320563add6c45f022217e477dcf78447f84e9
Maxwell <maxwell@protonmail.com>
2018-11-12 15:36:41 +1300

    Added linter

--- public/js/room.js
+++ public/js/room.js
@@ -1,107 +1,114 @@
+"use strict";
+
 const socket = io();
 
 // Elements
-const volumeSlider = document.querySelector('#volume');
-const seekSlider = document.querySelector('#seek');
-const pauseButton = document.querySelector('#pause');
-const playButton = document.querySelector('#play');
-const localFileField = document.querySelector('#localFile');
+const volumeSlider = document.querySelector("#volume");
+const seekSlider = document.querySelector("#seek");
+const pauseButton = document.querySelector("#pause");
+const playButton = document.querySelector("#play");
+const localFileField = document.querySelector("#localFile");
 
-const player = (() => {
-    const video = document.querySelector('#player');
-    
+const player = (function () {
+    const video = document.querySelector("#player");
+
     return {
-        setTime: (timeInSeconds) => video.currentTime = timeInSeconds,
-        
-        getTime: () => video.currentTime,
-        
-        pause: () => video.pause(),
-        
-        play: () => video.play(),
-        
-        fullScreen: () => {
-            if (video.mozRequestFullScreen)
-                video.mozRequestFullScreen();
-            else if (video.webkitRequestFullScreen)
-                video.webkitRequestFullScreen();
-            else
-                alert('Use right click > fullscreen.');
+        setTime: function (timeInSeconds) {
+            video.currentTime = timeInSeconds;
         },
-        
-        setVolume: (value) => video.volume = value,
-        
-        playFile: (file) => {
+
+        getTime: () => video.currentTime,
+
+        pause: () => video.pause(),
+
+        play: () => video.play(),
+
+        fullScreen: function () {
+            if (video.mozRequestFullScreen) {
+                video.mozRequestFullScreen();
+            } else if (video.webkitRequestFullScreen) {
+                video.webkitRequestFullScreen();
+            } else {
+                alert("Use right click > fullscreen.");
+            }
+        },
+
+        setVolume: function (value) {
+            video.volume = value;
+        },
+
+        playFile: function (file) {
             if (!file && video.canPlayType(file.type)) {
                 alert("You cannot play this file.");
             } else {
                 video.src = URL.createObjectURL(file);
             }
         },
-        
+
         getDuration: () => video.duration
     };
-})();
+}());
 
-volumeSlider.addEventListener('change', () => player.setVolume(volumeSlider.value));
+volumeSlider.addEventListener("change", () => player.setVolume(volumeSlider.value));
 
-localFileField.addEventListener('change', () => {
+localFileField.addEventListener("change", function () {
     const file = localFileField.files[0];
     player.playFile(file);
-    socket.emit('seek', {"time":player.getTime()});
+    socket.emit("seek", {"time": player.getTime()});
 });
 
 
 // Networking
 
 // Send pause
-pauseButton.addEventListener('click', () => {
+pauseButton.addEventListener("click", function () {
     player.pause();
-    socket.emit('pause', {"time": player.getTime()});
+    socket.emit("pause", {"time": player.getTime()});
 });
 
 // Recv pause
-socket.on('pause', (payload) => {
+socket.on("pause", function (payload) {
     player.setTime(payload.time);
     player.pause();
 });
 
 // Send play
-playButton.addEventListener('click', () => {
+playButton.addEventListener("click", function () {
     player.play();
-    socket.emit('play', {"time": player.getTime()});
+    socket.emit("play", {"time": player.getTime()});
 });
 
 // Recv play
-socket.on('play', (payload) => {
+socket.on("play", function (payload) {
     player.setTime(payload.time);
     player.play();
 });
 
 // Send seek
-seekSlider.addEventListener('change', () => {
+seekSlider.addEventListener("change", function () {
     console.log(seekSlider.value);
     console.log(player.getDuration());
-    
+
     const time = seekSlider.value * player.getDuration();
     player.setTime(time);
-    socket.emit('seek', {"time": time});
+    socket.emit("seek", {"time": time});
 });
 
 // Recv seek
-socket.on('seek', (payload) => {
+socket.on("seek", function (payload) {
     player.setTime(payload.time);
 });
 
 
-const readTimeToSeek = () => {
+function readTimeToSeek() {
     const duration = player.getDuration();
-    
+
     if (duration > 0) {
         seekSlider.value = player.getTime() / player.getDuration();
     } else {
         seekSlider.value = 0;
     }
-    
+
     setTimeout(readTimeToSeek, 250);
 }
 
